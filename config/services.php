30b86aa9af3b11857caa2de4f6f6e54c687a9f01
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-06-19 05:06:10 +0200

    add `trello` array

--- config/services.php
+++ config/services.php
@@ -24,6 +24,10 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'trello' => [
+        'key' => env('TRELLO_API_KEY'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
