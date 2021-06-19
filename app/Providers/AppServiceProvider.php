ae9b89166b2ea06ab22e37fc6759c4d70002c96a
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-06-19 05:09:21 +0200

    share current view name WITH studly case

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Str;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('*', function ($view) {
+            $viewName = Str::studly(Str::after($view->getName(), '.'));
+            view()->share('view_name', $viewName);
+        });
     }
 }
