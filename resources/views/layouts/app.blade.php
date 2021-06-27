c462235a496c33c638b9dfba450bd9df827394c5
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-06-27 16:44:34 +0200

    change the default value of the title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $view_name }} | {{ env('APP_NAME', 'Laravel-Trello') }}</title>
+    <title>{{ $view_name }} | {{ env('APP_NAME', 'Laravel Trello') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
